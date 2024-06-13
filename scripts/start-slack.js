fece906792e9deea657a702b2ff60c50b942adc4
developertom <developertom01@github.com>
2024-06-13 13:35:06 +0000

    Added slack app  script

---
+++ scripts/start-slack.js
@@ -0,0 +1,15 @@
+import bolt from '@slack/bolt';
+import pino from 'pino';
+
+const logger = pino();
+
+// Initializes your app with your bot token and signing secret
+const app = new bolt.App({
+    token: process.env.SLACK_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+
+await app.start(process.env.SLACK_PORT)
+
+logger.info('⚡️ Bolt app is running!');
