b211e361b773c03d513c8348e3dda2798f7539c8
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2024-07-20 15:36:59 -0700

    Only access environment variables in entry point files
    Also instantiate the `slack` client in the dev server,
    as it will later also be instantiated in the Netlify
    functions, but e.g. without `socketMode` enabled.

--- scripts/dev-server.js
+++ scripts/dev-server.js
@@ -1,20 +1,29 @@
+// @ts-check
+
+import bolt from "@slack/bolt";
 import { cleanEnv, str, num } from "envalid";
 import AppWebhookRelay from "github-app-webhook-relay-polling";
 import { App, Octokit } from "octokit";
 import { pino } from "pino";
 
 import githubApp from "../github-app.js";
-import { startSlackApp } from "./start-slack.js";
+import slackApp from "../slack-app.js";
 
 const env = cleanEnv(process.env, {
+  // GitHub App credentials
   GITHUB_APP_ID: num(),
   GITHUB_APP_PRIVATE_KEY: str(),
+
+  // Slack App credentials
+  SLACK_BOT_TOKEN: str(),
+  SLACK_APP_TOKEN: str(),
+  SLACK_SIGNING_SECRET: str(),
 });
 const log = pino();
 const octokitLog = log.child({ name: "octokit" });
 
 // instantiate the Octokit app
-const app = new App({
+const githubAppClient = new App({
   appId: env.GITHUB_APP_ID,
   privateKey: env.GITHUB_APP_PRIVATE_KEY,
   webhooks: {
@@ -35,14 +44,15 @@ const app = new App({
 
 // verify credentials and say hi
 // https://docs.github.com/rest/apps/apps#get-the-authenticated-app
-const { data: appInfo } = await app.octokit.request("GET /app");
+const { data: appInfo } = await githubAppClient.octokit.request("GET /app");
 log.info({ slug: appInfo.slug, url: appInfo.html_url }, `Authenticated`);
 
-// register webhook handlers
-await githubApp(app);
+// register GitHub webhook handlers
+await githubApp(githubAppClient);
 
+// receive webhooks by pulling
 const relay = new AppWebhookRelay({
-  app,
+  app: githubAppClient,
 });
 
 relay.on("error", (error) => {
@@ -50,4 +60,17 @@ relay.on("error", (error) => {
 });
 
 relay.start();
-startSlackApp(log)
+
+// start slack app
+// Initializes your app with your bot token and signing secret
+const slackAppClient = new bolt.App({
+  token: env.SLACK_BOT_TOKEN,
+  appToken: env.SLACK_APP_TOKEN,
+  signingSecret: env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+});
+
+await slackAppClient.start();
+log.info("⚡️ Bolt app is running!");
+
+slackApp(slackAppClient, log);
