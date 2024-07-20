36a9ac937c4d41726becc70d99c0fc60a4771c5d
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2024-07-20 15:55:49 -0700

    style: prettier

--- netlify/functions/webhooks.js
+++ netlify/functions/webhooks.js
@@ -1,10 +1,9 @@
-import { App, Octokit } from 'octokit';
-import githubApp from '../../github-app';
-import pino from 'pino';
-
+import { App, Octokit } from "octokit";
+import githubApp from "../../github-app";
+import pino from "pino";
 
 const log = pino();
-const octokitLog = log.child({ name: 'octokit' });
+const octokitLog = log.child({ name: "octokit" });
 
 let app;
 let setupAppError;
@@ -18,18 +17,18 @@ async function setupApp() {
   if (app) return app;
   if (setupAppError) throw setupAppError;
   try {
-      app = new App({
+    app = new App({
       appId: process.env.GITHUB_APP_ID,
       privateKey: process.env.GITHUB_APP_PRIVATE_KEY,
       oauth: {
         clientId: process.env.GITHUB_OAUTH_CLIENT_ID,
-        clientSecret: process.env.GITHUB_OAUTH_CLIENT_SECRET
+        clientSecret: process.env.GITHUB_OAUTH_CLIENT_SECRET,
       },
       webhooks: {
         secret: process.env.GITHUB_WEBHOOK_SECRET,
       },
       Octokit: Octokit.defaults({
-        userAgent: 'gr2m/github-app-slack-demo',
+        userAgent: "gr2m/github-app-slack-demo",
       }),
       log: {
         debug: octokitLog.debug.bind(octokitLog),
@@ -39,18 +38,18 @@ async function setupApp() {
       },
     });
 
-    const { data: appInfo } = await app.octokit.request('GET /app');
+    const { data: appInfo } = await app.octokit.request("GET /app");
     log.info({ slug: appInfo.slug, url: appInfo.html_url }, `Authenticated`);
 
     await githubApp(app);
-    
-    return app
+
+    return app;
   } catch (error) {
-    log.error(error, 'Failed to set up app');
+    log.error(error, "Failed to set up app");
     setupAppError = error;
     throw error;
   }
-};
+}
 
 /**
  * Netlify function to handle webhook event requests from GitHub
@@ -58,19 +57,19 @@ async function setupApp() {
  * @param {import("@netlify/functions").HandlerEvent} event
  * @param {import("@netlify/functions").HandlerContext} context
  */
-export async function handler (event, context) {
-  if (event.httpMethod !== "POST"){
+export async function handler(event, context) {
+  if (event.httpMethod !== "POST") {
     return {
-      statusCode: /* The `405` status code in HTTP indicates that the method used in the request is not
+      /* The `405` status code in HTTP indicates that the method used in the request is not
       allowed for the specified resource. In the provided code snippet, when the
       `handler` function is called with an HTTP method other than `POST`, it returns a
       response with a status code of `405` along with an error message indicating that
       the method is not allowed for that endpoint. */
-      405,
-      body: JSON.stringify({ error: 'Method not allowed' }),
-    }
+      statusCode: 405,
+      body: JSON.stringify({ error: "Method not allowed" }),
+    };
   }
-  
+
   try {
     let didTimeout = false;
     const timeout = setTimeout(() => {
@@ -81,39 +80,40 @@ export async function handler (event, context) {
 
     const app = await setupApp();
     await app.webhooks.verifyAndReceive({
-      id: event.headers["X-GitHub-Delivery"] ||
-      event.headers["x-github-delivery"],
+      id:
+        event.headers["X-GitHub-Delivery"] ||
+        event.headers["x-github-delivery"],
       name: event.headers["X-GitHub-Event"] || event.headers["x-github-event"],
-      signature: event.headers["X-Hub-Signature-256"] ||
-      event.headers["x-hub-signature-256"],
-      payload: JSON.parse(event.body)
-    })
+      signature:
+        event.headers["X-Hub-Signature-256"] ||
+        event.headers["x-hub-signature-256"],
+      payload: JSON.parse(event.body),
+    });
     clearTimeout(timeout);
 
-    app.webhooks.on('error', (error) => {
-      log.error(error, 'Webhook error');
+    app.webhooks.on("error", (error) => {
+      log.error(error, "Webhook error");
     });
 
     //TODO: Implement the event handler
-    app.webhooks.on('event', (event) => {
-      log.info({ event }, 'Webhook received');
-    }
-    );
-    
-    if (didTimeout) return {
-      statusCode: 202,
-      body: JSON.stringify({ ok: true }),
-    };
+    app.webhooks.on("event", (event) => {
+      log.info({ event }, "Webhook received");
+    });
+
+    if (didTimeout)
+      return {
+        statusCode: 202,
+        body: JSON.stringify({ ok: true }),
+      };
 
     return {
       statusCode: 202,
-    }
-
+    };
   } catch (error) {
-    log.error(error, 'Handler error');
+    log.error(error, "Handler error");
     clearTimeout(timeout);
 
-    const err = Array.from((error).errors)[0];
+    const err = Array.from(error.errors)[0];
     const errorMessage = err.message
       ? `${err.name}: ${err.message}`
       : "Error: An Unspecified error occurred";
@@ -122,7 +122,6 @@ export async function handler (event, context) {
     return {
       statusCode,
       body: errorMessage,
-    }
-    
+    };
   }
-};
+}
