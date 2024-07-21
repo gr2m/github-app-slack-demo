6d5f48d66d98f91912aef1f36ec3ca79a1395c36
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2024-07-20 17:07:32 -0700

    feat: add log to `/api/health`

--- netlify/functions/health.js
+++ netlify/functions/health.js
@@ -1,3 +1,7 @@
+import pino from "pino";
+
+const log = pino();
+const healthLog = log.child({ name: "health" });
 
 /**
  * Netlify function for health check
@@ -5,9 +9,11 @@
  * @param {import("@netlify/functions").HandlerEvent} event
  * @param {import("@netlify/functions").HandlerContext} context
  */
-export const handler= async(event, context)=>{
-    return {
-        body: JSON.stringify({ ok:true}),
-        statusCode: 200
-    }
-}
+export const handler = async (event, context) => {
+  healthLog.info("Health check");
+
+  return {
+    body: JSON.stringify({ ok: true }),
+    statusCode: 200,
+  };
+};
