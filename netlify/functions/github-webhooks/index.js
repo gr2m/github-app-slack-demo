1c2b355fa4007ace990d98417d97d2dee68ee8b1
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2024-07-20 18:22:12 -0700

    attempt to make ESM and latest octokit work ...

--- netlify/functions/github-webhooks/index.js
+++ netlify/functions/github-webhooks/index.js
@@ -2,7 +2,7 @@ import { cleanEnv, str, num } from "envalid";
 import { App, Octokit } from "octokit";
 import pino from "pino";
 
-import githubApp from "../../github-app";
+import githubApp from "../../../github-app";
 
 const env = cleanEnv(process.env, {
   // GitHub App credentials
