71deb819389c2b6aaba529682e647c767d674427
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2024-07-21 15:23:14 -0700

    export `healthLog` for testing

--- netlify/functions/health/index.js
+++ netlify/functions/health/index.js
@@ -1,19 +1,16 @@
 import pino from "pino";
 
-const log = pino();
-const healthLog = log.child({ name: "health" });
+// export for testing
+export const healthLog = pino().child({ name: "health" });
 
 /**
  * Netlify function for health check
- *
- * @param {import("@netlify/functions").HandlerEvent} event
- * @param {import("@netlify/functions").HandlerContext} context
  */
-export const handler = async (event, context) => {
+export async function handler() {
   healthLog.info("Health check");
 
   return {
     body: JSON.stringify({ ok: true }),
     statusCode: 200,
   };
-};
+}
