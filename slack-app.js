d508e60343b8e02e8833ab5f7905b7c42022620b
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2024-07-20 19:23:15 -0700

    clean up slack app

--- slack-app.js
+++ slack-app.js
@@ -8,17 +8,17 @@ export default async function slackApp(app, log) {
   app.event("app_home_opened", async ({ event, client, context }) => {
     log.info("app_home_opened event received");
 
-    /* view.publish is the method that your app uses to push a view to the Home tab */
-    const result = await client.views.publish({
-      /* the user that opened your app's app home */
+    // view.publish is the method that your app uses to push a view to the Home tab
+    await client.views.publish({
+      // the user that opened your app's app home
       user_id: event.user,
 
-      /* the view object that appears in the app home*/
+      // the view object that appears in the app home
       view: {
         type: "home",
         callback_id: "home_view",
 
-        /* body of the view */
+        // body of the view
         blocks: [
           {
             type: "section",
