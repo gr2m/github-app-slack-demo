e6a939a3f821bdb4a968cea3984b66c580100bbb
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2024-09-18 21:32:53 -0700

    remove debug shizzle from health endpoint

--- netlify/functions/health/index.js
+++ netlify/functions/health/index.js
@@ -1,20 +1,5 @@
 import pino from "pino";
 
-import { cleanEnv, str } from "envalid";
-
-import { getInstallationStore } from "../../../lib/slack-installation-store.js";
-
-const env = cleanEnv(process.env, {
-  // netlify environment variables
-  SITE_ID: str(),
-  NETLIFY_PERSONAL_ACCESS_TOKEN: str(),
-});
-
-const boltInstallationStore = getInstallationStore({
-  siteID: env.SITE_ID,
-  token: env.NETLIFY_PERSONAL_ACCESS_TOKEN,
-});
-
 // export for testing
 export const healthLog = pino().child({ function: "health" });
 
@@ -23,8 +8,6 @@ export const healthLog = pino().child({ function: "health" });
  */
 export async function handler() {
   healthLog.info("Health check");
-
-  await boltInstallationStore.fetchInstallation({ teamId: "T06SS47EH52" });
 
   return {
     body: JSON.stringify({ ok: true }),
